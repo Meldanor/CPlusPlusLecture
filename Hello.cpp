3090b9e287e341afc21c9175b119a5feb50ca6c6
Meldanor <Bhaals@gmx.de>
2013-04-09 10:41:38 +0200

    Initial commit

---
+++ Hello.cpp
@@ -0,0 +1,17 @@
+/*
+ * Hello.cpp
+ *
+ *  Created on: 09.04.2013
+ *      Author: Meldanor
+ */
+
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+int main() {
+	cout << "Hello World!" << endl;
+	return 0;
+}
+
